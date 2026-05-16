32879b6ba2eeb007449cceac3b30241d17732d56
Ahmad Munim Che Yaziz <ahmadmunimcheyaziz@Ahmads-MacBook-Air.local>
2026-05-17 00:10:35 +0800

    Finished Update Dashboard

--- Program.cs
+++ Program.cs
@@ -1,24 +1,17 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddRazorPages();
+// 1. Tambah servis untuk Controller dan Views
+builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthorization();
 
-app.MapRazorPages();
+// 2. Tetapkan laluan utama ke HomeController
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
